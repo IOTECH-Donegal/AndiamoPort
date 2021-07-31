5e88d3a2594f035234277c922c8b30ed99f7ba07
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2021-07-31 10:44:24 +0100

    Update autoexec.sh

--- autoexec.sh
+++ autoexec.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # by: JOR
 # Date: 30JUL21
-# Function: autoexec.bat for Linux
-# Leave this script in /home/pi
-# Script: autoexec.sh
-
+# Function: autoexec for Linux
+# Copy this script to /home/pi/
 # To automate running this script, add the following to /etc/rc.local BEFORE the line exit 0
 # sudo /home/pi/autoexec.sh
+# Script: autoexec.sh
+
 
 HOMEPATH="/home/pi"
 
-echo "Setting time from GPS"
-sleep 20
-sudo python3 $HOMEPATH/SetUTC/SetUTC.py 2>$HOMEPATH/SetUTC/SetUTC.err
+#echo "Setting time from GPS"
+#sleep 20
+#sudo python3 $HOMEPATH/SetUTC/SetUTC.py 2>$HOMEPATH/SetUTC/SetUTC.err
 
 python3 $HOMEPATH/SerialMux/SerialMux.py
